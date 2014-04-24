5bdeb1d3ccb6262bf8bc7d07733e0f2a8603dcbb
n2code <n2code@users.noreply.github.com>
2014-04-24 15:00:26 +0200

    first version, to be improved

---
+++ DropboxChatPrototype/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DropboxChatPrototype
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.sendbutton = new System.Windows.Forms.Button();
+			this.nickname = new System.Windows.Forms.TextBox();
+			this.input = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.chatview = new System.Windows.Forms.RichTextBox();
+			this.prechatview = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// sendbutton
+			// 
+			this.sendbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.sendbutton.Location = new System.Drawing.Point(469, 281);
+			this.sendbutton.Name = "sendbutton";
+			this.sendbutton.Size = new System.Drawing.Size(73, 22);
+			this.sendbutton.TabIndex = 1;
+			this.sendbutton.Text = "Login";
+			this.sendbutton.UseVisualStyleBackColor = true;
+			this.sendbutton.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// nickname
+			// 
+			this.nickname.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.nickname.Location = new System.Drawing.Point(14, 282);
+			this.nickname.Name = "nickname";
+			this.nickname.Size = new System.Drawing.Size(74, 20);
+			this.nickname.TabIndex = 2;
+			this.nickname.Text = "Nickname";
+			this.nickname.TextChanged += new System.EventHandler(this.NicknameTextChanged);
+			this.nickname.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox2KeyDown);
+			// 
+			// input
+			// 
+			this.input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.input.Enabled = false;
+			this.input.Location = new System.Drawing.Point(92, 282);
+			this.input.Name = "input";
+			this.input.Size = new System.Drawing.Size(371, 20);
+			this.input.TabIndex = 3;
+			this.input.TextChanged += new System.EventHandler(this.InputTextChanged);
+			this.input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox3KeyDown);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// chatview
+			// 
+			this.chatview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+									| System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.chatview.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.chatview.Location = new System.Drawing.Point(9, 9);
+			this.chatview.Name = "chatview";
+			this.chatview.ReadOnly = true;
+			this.chatview.Size = new System.Drawing.Size(533, 263);
+			this.chatview.TabIndex = 4;
+			this.chatview.Text = "";
+			this.chatview.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.ChatviewLinkClicked);
+			this.chatview.Resize += new System.EventHandler(this.ChatviewResize);
+			// 
+			// prechatview
+			// 
+			this.prechatview.Location = new System.Drawing.Point(-1, 0);
+			this.prechatview.Name = "prechatview";
+			this.prechatview.Size = new System.Drawing.Size(125, 17);
+			this.prechatview.TabIndex = 5;
+			this.prechatview.Text = "";
+			this.prechatview.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.sendbutton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Silver;
+			this.ClientSize = new System.Drawing.Size(554, 309);
+			this.Controls.Add(this.prechatview);
+			this.Controls.Add(this.chatview);
+			this.Controls.Add(this.input);
+			this.Controls.Add(this.nickname);
+			this.Controls.Add(this.sendbutton);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(570, 347);
+			this.Name = "MainForm";
+			this.Text = "DropChat";
+			this.Shown += new System.EventHandler(this.MainFormShown);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.RichTextBox prechatview;
+		private System.Windows.Forms.RichTextBox chatview;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.TextBox input;
+		private System.Windows.Forms.TextBox nickname;
+		private System.Windows.Forms.Button sendbutton;
+	}
+}
---
+++ DropboxChatPrototype/MainForm.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Globalization;
+
+namespace DropboxChatPrototype
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string chatpath;
+		string pathsep;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			pathsep = System.IO.Path.DirectorySeparatorChar.ToString();
+			chatpath = Application.StartupPath+pathsep+"dropchat";	
+			try
+			{
+				Directory.CreateDirectory(chatpath);
+			} catch {}
+			updatechat();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (sendbutton.Text=="Login")
+			{
+				if(nickname.Text.Trim() == "" || nickname.Text.Trim() == "Nickname") 
+				{
+					MessageBox.Show("You have to enter a nickname to login!");
+				}
+				else
+				{
+					nickname.Enabled = false;
+					nickname.Text = nickname.Text.Trim().Replace(":","").Replace("#","");
+					input.Enabled = true;
+					sendbutton.Text = "Send";
+					input.Text = "";
+					input.Focus();
+				}
+			}
+			/*else if (nickname.Text=="SECUREFILE" && File.Exists(input.Text))
+			{
+				MessageBox.Show(WriteSecureFile(Path.GetDirectoryName(input.Text),Path.GetFileName(input.Text),File.ReadAllText(input.Text,Encoding.UTF8)),"OUTPUT:");
+				input.Focus();
+			}*/
+			else
+			{
+				input.Text = input.Text.TrimEnd();
+				if (input.Text.TrimStart().StartsWith("#")) input.Text = input.Text.Replace("#","");
+				if(input.Text.Trim() == "") return;
+				WriteSecureFile(chatpath,DateTime.Now.ToString("yyyyMMddHHmmssfff")+".chat",nickname.Text+": "+input.Text);
+				input.Text = "";
+				input.Focus();
+				updatechat();
+			}
+		}
+		
+		string WriteSecureFile(string path, string filename, string content)
+		{
+			try {
+				filename = filename.ToUpper();
+	            using (MD5 md5Hash = MD5.Create())
+	            {
+	                string hash = GetMd5Hash(md5Hash, "dropchat"+filename+content);
+	                string target = path+pathsep+filename+"."+hash.Substring(14,7).ToUpper();
+	                File.WriteAllBytes(target,Encoding.UTF8.GetBytes(content));
+	                return target;
+	            }
+			} catch  { MessageBox.Show("ERROR: Message file could not be written."); }
+			return "";
+		}
+
+		bool IsValidFile(MD5 md5Hash, string fullpath, string content)
+		{
+			string hash = GetMd5Hash(md5Hash, "dropchat"+Path.GetFileNameWithoutExtension(fullpath)+content);
+			if (hash.ToUpper().Substring(14,7)==Path.GetExtension(fullpath).ToUpper().Replace(".",""))
+			{
+				return true;
+			}
+			else
+			{
+				return false;	
+			}
+		}
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+			byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+		
+		void TextBox3KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				this.WindowState = FormWindowState.Minimized;
+				e.Handled = true;
+				e.SuppressKeyPress = true;
+			}
+		}
+		
+		void updatechat()
+		{
+			string[] chat;
+			try
+			{
+				chat = Directory.GetFiles(chatpath);
+				Array.Sort(chat);
+			} catch { return; }
+			
+			prechatview.Clear();
+			prechatview.Font = new Font("Arial", 8f, FontStyle.Italic);
+			prechatview.AppendText("DropChat - proof-of-concept of a simple chat using textfiles and Dropbox as a \"network component\"");
+
+			using (MD5 md5Hash = MD5.Create())
+            {
+				foreach (string datei in chat)
+				{
+					string nachricht;
+					try
+					{
+						nachricht = File.ReadAllText(datei,Encoding.UTF8);
+					} catch { continue; }
+					if (!IsValidFile(md5Hash,datei,nachricht)) continue;
+					
+					prechatview.AppendText(Environment.NewLine);
+					int start = prechatview.Text.Length;
+					
+					string nurdatei = Path.GetFileName(datei);
+					DateTime datum;
+					if (nurdatei.Length>="yyyyMMddHHmmssfff".Length && DateTime.TryParseExact(nurdatei.Substring(0,"yyyyMMddHHmmssfff".Length),"yyyyMMddHHmmssfff",CultureInfo.InvariantCulture,DateTimeStyles.None, out datum))
+					{
+						prechatview.AppendText(datum.ToString("(HH:mm:ss) "));
+						prechatview.SelectionStart = start;
+						prechatview.SelectionLength = "(HH:mm:ss) ".Length;
+						prechatview.SelectionFont = new Font("Arial", 8f, FontStyle.Regular);
+						prechatview.SelectionLength = 0;
+						start += "(HH:mm:ss) ".Length;
+					}
+	
+					bool istspezial = nachricht.Trim().StartsWith("#");
+					string schriftart = (istspezial?"Courier New":"Arial");
+					
+					FontStyle schrifttyp = (istspezial?FontStyle.Italic:FontStyle.Regular);
+					prechatview.AppendText(nachricht);
+	
+					prechatview.SelectionStart = start;
+					prechatview.SelectionLength = nachricht.Length;
+					prechatview.SelectionFont = new Font(schriftart, 11f, schrifttyp);
+					prechatview.SelectionLength = 0;
+					
+					if (nachricht.IndexOf(':')>=0 && !istspezial)
+					{
+						prechatview.SelectionStart = start;
+						prechatview.SelectionLength = nachricht.IndexOf(':')+1;
+						prechatview.SelectionFont = new Font(schriftart, 11f, FontStyle.Bold);
+						prechatview.SelectionLength = 0;
+					}
+				}
+			}
+			if (chatview.Rtf != prechatview.Rtf)
+			{
+				chatview.Rtf = prechatview.Rtf;
+				chatview.SelectionStart = chatview.Text.Length;
+				chatview.ScrollToCaret();
+			}
+
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			updatechat();
+		}
+		
+		void MainFormShown(object sender, EventArgs e)
+		{
+			nickname.SelectionStart = 0;
+			nickname.SelectionLength = nickname.Text.Length;
+			nickname.Focus();
+		}
+		
+		void TextBox2KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter) sendbutton.PerformClick();
+		}
+		
+		void ChatviewLinkClicked(object sender, LinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start(e.LinkText);
+		}
+		
+		void NicknameTextChanged(object sender, EventArgs e)
+		{
+		}
+		
+		void MainFormKeyDown(object sender, KeyEventArgs e)
+		{
+		}
+		
+		void ChatviewResize(object sender, EventArgs e)
+		{
+			chatview.Refresh();
+			chatview.SelectionStart = chatview.Text.Length;
+			chatview.ScrollToCaret();
+		}
+		
+		void InputTextChanged(object sender, EventArgs e)
+		{
+		}
+	}
+}
